e58ffdad4e4bf109d167df8a5c5c02300694df9c
Muhammad Obaid Ullah Khan <mobaidkhan2@gmail.com>
2024-05-25 02:07:13 +0500

    update recipe page UI

--- frontend/src/pages/RecipesPage.jsx
+++ frontend/src/pages/RecipesPage.jsx
@@ -1,9 +1,22 @@
-import { Spinner } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Container,
+    Flex,
+    Input,
+    Spinner,
+    Text,
+    Grid,
+    GridItem,
+    useColorModeValue,
+    Icon,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import { useRecoilState } from "recoil";
 import postsAtom from "../atoms/postsAtom";
 import FeaturedPost from "../components/FeaturedPost";
+import { SearchX } from "lucide-react";
 
 const HomePage = () => {
     const [posts, setPosts] = useRecoilState(postsAtom);
@@ -12,6 +25,7 @@ const HomePage = () => {
     const [searchedPosts, setSearchedPosts] = useState([]);
     const [searchClicked, setSearchClicked] = useState(false);
     const showToast = useShowToast();
+
     useEffect(() => {
         const getFeedPosts = async () => {
             setLoading(true);
@@ -23,7 +37,6 @@ const HomePage = () => {
                     showToast("Error", data.error, "error");
                     return;
                 }
-                console.log(data);
                 setPosts(data);
             } catch (error) {
                 showToast("Error", error.message, "error");
@@ -45,7 +58,6 @@ const HomePage = () => {
 
             const res = await fetch(`/api/posts/search?q=${searchQuery}`);
             const searchData = await res.json();
-            console.log("search data from handle search", searchData);
             if (searchData.error) {
                 showToast("Error", searchData.error, "error");
                 return;
@@ -63,86 +75,107 @@ const HomePage = () => {
         setSearchQuery("");
         setSearchedPosts([]);
         setSearchClicked(false);
-    }
+    };
+
     return (
-        <div>
+        <Box>
+            <Box maxW="1400px" mx="auto" py={4}>
+                <form onSubmit={handleSearch} >
+                    <Flex maxW="600px" mx={"auto"}>
+                        <Input
+                            type="text"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                            bg="whitesmoke"
+                            rounded="full"
+                            py={2}
+                            px={6}
+                            focusBorderColor="green.300"
+                            placeholder="Search Recipe..."
+                            w="full"
+                            borderColor={"green.200"}
+                        />
+                        <Button
+                            onClick={handleSearch}
+                            bg="green.400"
+                            color="white"
+                            fontWeight="semibold"
+                            py={2}
+                            px={6}
+                            ml={2}
+                            rounded="full"
+                            _hover={{ bg: "green.300" }}
+                        >
+                            Search
+                        </Button>
+                        <Button
+                            onClick={clearSearch}
+                            bg="red.200"
+                            color="white"
+                            fontWeight="bold"
+                            py={2}
+                            px={4}
+                            ml={2}
+                            rounded="full"
+                            _hover={{ bg: "red.300" }}
+                        >
+                            <Icon as={SearchX} w={5} h={5} color={"blackAlpha.800"} fill={'blackAlpha.200'} />
 
-            <div className="max-w-lg px-6 py-4 mx-auto">
-                <form onSubmit={handleSearch} className="flex items-center">
-                    <input
-                        type="text"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        className="w-full px-4 py-2 bg-gray-100 border rounded-md border-slate-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Search..."
-                    />
-                    <button
-                        onClick={handleSearch}
-                        className="px-4 py-2 ml-2 font-bold text-white bg-black rounded hover:bg-gray-500 "
-                    >
-                        Search
-                    </button>
-                    <button
-                        onClick={clearSearch}
-                        className="px-4 py-2 ml-2 font-bold text-white bg-red-500 rounded hover:bg-red-700"
-                    >
-                        Clear
-                    </button>
+
+                        </Button>
+                    </Flex>
                 </form>
-            </div>
+            </Box>
 
-            <div className="">
+            <Container maxW="container.lg">
                 {loading && searchQuery.length > 0 && searchedPosts.length === 0 && (
-                    <div className="">
+                    <Flex justify="center" mt={4}>
                         <Spinner size="xl" />
-                    </div>
-
+                    </Flex>
                 )}
 
-                {
-                    !loading && searchedPosts.length === 0 && (
-                        <div>
-                            <h1>No results found</h1>
-                        </div>
-                    )
-                }
-
+                {!loading && searchQuery.length > 0 && searchedPosts.length === 0 && (
+                    <Flex justify="center" mt={4}>
+                        <Text>No results found</Text>
+                    </Flex>
+                )}
 
                 {searchedPosts.length > 0 && (
-                    <div className="container py-8 mx-auto">
-                        <h2 className="mb-4 text-3xl font-semibold">Searched Results</h2>
-                        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+                    <Box py={8}>
+                        <Text mb={4} fontSize="3xl" fontWeight="semibold">Searched Results</Text>
+                        <Grid templateColumns="repeat(auto-fit, minmax(250px, 1fr))" gap={6} maxWidth={"22rem"}>
                             {searchedPosts.map((post) => (
-                                <FeaturedPost
-                                    key={post._id}
-                                    post={post}
-                                    postedBy={post.postedBy}
-                                />
+                                <GridItem key={post._id} >
+                                    <FeaturedPost post={post} postedBy={post.postedBy} />
+                                </GridItem>
                             ))}
-                        </div>
-                    </div>
+                        </Grid>
+                    </Box>
                 )}
-            </div>
+            </Container>
 
-            <div className="container py-8 mx-auto">
-                <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-4">
-                    {!loading && posts.length === 0 && (
-                        <h1>Follow some users to see the feed</h1>
-                    )}
+            <Container maxW="container.lg" py={8}>
+                {loading && (
+                    <Flex justify="center" mt={4}>
+                        <Spinner size="xl" />
+                    </Flex>
+                )}
 
-                    {loading && (
-                        <div className="">
-                            <Spinner size="xl" />
-                        </div>
-                    )}
+                {!loading && posts.length === 0 && (
+                    <Flex justify="center" mt={4}>
+                        <Text>Follow some users to see the feed</Text>
+                    </Flex>
+                )}
 
+                <Grid templateColumns="repeat(auto-fit, minmax(250px, 1fr))" gap={6} mt={6}>
                     {posts.map((post) => (
-                        <FeaturedPost key={post._id} post={post} postedBy={post.postedBy} />
+                        <GridItem key={post._id}>
+                            <FeaturedPost post={post} postedBy={post.postedBy} />
+                        </GridItem>
                     ))}
-                </div>
-            </div>
-
-        </div>
+                </Grid>
+            </Container>
+        </Box>
     );
 };
 
